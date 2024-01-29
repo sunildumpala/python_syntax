2d95df47718d5e86f4716109ca06321050737981
sunildumpala <dumpala@gmail.com>
2024-01-29 10:18:39 -0800

    words working

--- words.py
+++ words.py
@@ -1,3 +1,9 @@
-words_list = ["hello","hey", "goodbye", "yo", "yes"]
-for word in words_list:
-  print(word.upper())
+def print_upper_words(words_list, must_start_with):
+    """
+      For an array of words, print them in uppercase, only if they begin with a letter in must_start_with list.
+    """
+    for word in words_list:
+        if word[0] in must_start_with:
+            print(word.upper())
+
+print_upper_words(["hello","hey", "goodbye", "yo", "yes"], {"h", "y"})
